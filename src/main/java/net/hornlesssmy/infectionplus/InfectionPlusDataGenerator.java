8cc2cc1bfd8ab264e96ea2205ed1162f99d86257
House <house@Houses-iMac.cogeco.local>
2025-04-07 21:53:46 -0400

    Initial commit

---
+++ src/main/java/net/hornlesssmy/infectionplus/InfectionPlusDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hornlesssmy.infectionplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfectionPlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
